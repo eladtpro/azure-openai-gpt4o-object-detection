56d328b079f7111bb4b3c1e2ae2890222548006f
eladtpro <eladt.pro@gmail.com>
2024-07-30 11:02:11 +0300

    detect_objects

--- function_app.py
+++ function_app.py
@@ -27,28 +27,28 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         )
 
 
-# @app.function_name(name="detect_objects")
-# @app.service_bus_queue_trigger(arg_name="input", queue_name="input", connection="ServiceBusConnection")
-# # @app.service_bus_topic_output(arg_name="output", queue_name="output", connection="ServiceBusConnection")
-# def detect_objects(input: func.ServiceBusMessage, output: func.Out[str]):
-#     event_string = input.get_body().decode('utf-8')
-#     # event = {"topic":"/subscriptions/977171a9-6bfd-49c4-a496-018d3312466e/resourceGroups/azure-openai/providers/Microsoft.Storage/storageAccounts/stfunctionsopenai","subject":"/blobServices/default/containers/sources/blobs/_636d5f92-d15d-4d13-905e-fd17b09aacc5.jpeg","eventType":"Microsoft.Storage.BlobCreated","id":"695addc9-401e-00db-75bb-e174ef061cf8","data":{"api":"PutBlob","clientRequestId":"87083d28-f4a8-482b-ae17-e41aae222009","requestId":"695addc9-401e-00db-75bb-e174ef000000","eTag":"0x8DCAFD2E44A1357","contentType":"image/jpeg","contentLength":256569,"blobType":"BlockBlob","url":"https://stfunctionsopenai.blob.core.windows.net/sources/_636d5f92-d15d-4d13-905e-fd17b09aacc5.jpeg","sequencer":"00000000000000000000000000009D950000000000501908","storageDiagnostics":{"batchId":"8acc52d3-6006-0075-00bb-e1d9fe000000"}},"dataVersion":"","metadataVersion":"1","eventTime":"2024-07-29T13:32:28.7820631Z"}
-#     logging.info('Python ServiceBus Queue trigger processed a message: %s', event_string)
-#     event = json.loads(event_string)
-#     image_url = event['data']['url']
-#     # Process the image_url as needed
-#     response = process_image(image_url)
+@app.function_name(name="detect_objects")
+@app.service_bus_queue_trigger(arg_name="input", queue_name="input", connection="ServiceBusConnection")
+# @app.service_bus_topic_output(arg_name="output", queue_name="output", connection="ServiceBusConnection")
+def detect_objects(input: func.ServiceBusMessage, output: func.Out[str]):
+    event_string = input.get_body().decode('utf-8')
+    # event = {"topic":"/subscriptions/977171a9-6bfd-49c4-a496-018d3312466e/resourceGroups/azure-openai/providers/Microsoft.Storage/storageAccounts/stfunctionsopenai","subject":"/blobServices/default/containers/sources/blobs/_636d5f92-d15d-4d13-905e-fd17b09aacc5.jpeg","eventType":"Microsoft.Storage.BlobCreated","id":"695addc9-401e-00db-75bb-e174ef061cf8","data":{"api":"PutBlob","clientRequestId":"87083d28-f4a8-482b-ae17-e41aae222009","requestId":"695addc9-401e-00db-75bb-e174ef000000","eTag":"0x8DCAFD2E44A1357","contentType":"image/jpeg","contentLength":256569,"blobType":"BlockBlob","url":"https://stfunctionsopenai.blob.core.windows.net/sources/_636d5f92-d15d-4d13-905e-fd17b09aacc5.jpeg","sequencer":"00000000000000000000000000009D950000000000501908","storageDiagnostics":{"batchId":"8acc52d3-6006-0075-00bb-e1d9fe000000"}},"dataVersion":"","metadataVersion":"1","eventTime":"2024-07-29T13:32:28.7820631Z"}
+    logging.info('Python ServiceBus Queue trigger processed a message: %s', event_string)
+    event = json.loads(event_string)
+    image_url = event['data']['url']
+    # Process the image_url as needed
+    response = process_image(image_url)
 
-#     if hasattr(response, 'choices'):
-#         print("response.choices is an array with one item or more")
-#         output.set(str(response.choices[0]))
-#     else:
-#         print("response.choices is not an array with one item or more")
-#         output.set("response.choices is not an array with one item or more")
+    if hasattr(response, 'choices'):
+        print("response.choices is an array with one item or more")
+        output.set(str(response.choices[0]))
+    else:
+        print("response.choices is not an array with one item or more")
+        output.set("response.choices is not an array with one item or more")
 
-#     json_string = str(response.choices[0])
-#     print(json_string)
-#     output.set(json_string)
+    json_string = str(response.choices[0])
+    print(json_string)
+    output.set(json_string)
 
 def process_image(image_url):
     response = client.chat.completions.create(
